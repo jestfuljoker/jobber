af0b76b3d293d681866d1950daf41ee608f6b091
Christofer Assis <chris.f.assis18@gmail.com>
2025-04-26 10:31:34 -0300

    fix: broken tests

--- apps/auth/src/app/auth/auth.resolver.spec.ts
+++ apps/auth/src/app/auth/auth.resolver.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { AuthResolver } from './auth.resolver';
+import { AuthResolver } from '~/app/auth/auth.resolver';
+import { AuthService } from '~/app/auth/auth.service';
 
 describe('AuthResolver', () => {
 	let resolver: AuthResolver;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [AuthResolver],
+			providers: [
+				AuthResolver,
+				{
+					provide: AuthService,
+					useValue: {},
+				},
+			],
 		}).compile();
 
 		resolver = module.get<AuthResolver>(AuthResolver);
--- apps/auth/src/app/auth/auth.service.spec.ts
+++ apps/auth/src/app/auth/auth.service.spec.ts
@@ -1,13 +1,30 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { AuthService } from './auth.service';
+import { AuthService } from '~/app/auth/auth.service';
+import { UsersService } from '~/app/users/users.service';
 
 describe('AuthService', () => {
 	let service: AuthService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [AuthService],
+			providers: [
+				AuthService,
+				{
+					provide: UsersService,
+					useValue: {},
+				},
+				{
+					provide: JwtService,
+					useValue: {},
+				},
+				{
+					provide: ConfigService,
+					useValue: {},
+				},
+			],
 		}).compile();
 
 		service = module.get<AuthService>(AuthService);
